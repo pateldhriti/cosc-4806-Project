6923839a867d1ce5cff9882bcf6167598aed21d6
pateldhriti <135610241+pateldhriti@users.noreply.github.com>
2025-07-25 01:29:31 +0000

    add header layout and change name from movie to public

---
+++ app/views/includes/header_public.php
@@ -0,0 +1,13 @@
+<DOCTYPE html>
+<html lang="en">  
+  <head>
+    <meta charset="UTF-8">
+<title> COSC Project Example</title>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
+
+  </head>  
+<body>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark px-4">
+    <a class="navbar-brand" href="/Movie">COSC4806 Project</a>
+  </nav>
+</body>
