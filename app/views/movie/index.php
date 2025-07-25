9a1fe3844a4ad8c79c564d5f72ed5b9c3dcc6cae
pateldhriti <135610241+pateldhriti@users.noreply.github.com>
2025-07-25 01:15:48 +0000

    Add movie search view

---
+++ app/views/movie/index.php
@@ -0,0 +1,9 @@
+<?php include_once 'app/views/includes/header_public.php'; ?>
+
+<div class ="container mt-5">
+  <h2 class="mb-4 text-center">Search for a movie</h2>"
+  <form action="/movie/search" method="post" class="d-flex justify-content-center">
+    <input type="text" name="movie" class="form-control w-50 me-2" placeholder="Enter movie title" required>
+    <button type="submit" class="btn btn-primary">Search</button>
+    </form>
+    </div>
