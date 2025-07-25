e5177a4b243ed967c8fc504722524321a5b9785f
pateldhriti <135610241+pateldhriti@users.noreply.github.com>
2025-07-25 18:20:56 +0000

    update the rating section inside the view

--- app/views/movie/show.php
+++ app/views/movie/show.php
@@ -4,8 +4,9 @@
   <h2><?= htmlspecialchars($data['title']) ?></h2>
   <div class= "row mt-3">
   <div class="col-md-4">
-    <img src="<?= $data['movie']['Poster'] ?>" class="img-fluid" alt="Poster">  
+  <img src="<?= $data['movie']['Poster'] ?>" class="img-fluid" alt="Poster">  
     </div>  
+    
     <div class="col-md-8">
       <p><strong>Year:</strong> <?= $data['movie']['Year'] ?></p>
       <p><strong>Genre:</strong> <?= $data['movie']['Genre'] ?></p>
@@ -15,6 +16,17 @@
 
       <hr>
       <h4>⭐ Rate this Movie</h4>
+      <form action="/index.php" method="post">  
+        <input type="hidden" name="url" value="Movie/saveRating">
+        <input type="hidden" name="title" value="<?= htmlspecialchars($data['title']) ?>">"
+      <div class="d-flex flex-wrap gap-2 mt-2">
+        <?php for ($i = 1; $i <= 5; $i++): ?>
+          <button type="submit" name="rating" value="<?= $i ?>" class="btn btn-outline-warning"><?= $i ?> Star</button>
+        <?php endfor; ?>
+        </div>
+          </form>
+
+        
       <?php for ($i = 1; $i <= 5; $i++): ?>
         <a href="/Movie/review/<?= urlencode($data['title']) ?>/<?= $i ?>" class="btn btn-outline-warning me-1"><?= $i ?> Star</a>
       <?php endfor; ?>
